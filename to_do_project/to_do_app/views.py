1e97103521abfafe8c8e59396642bcecd3a9467d
Xavier Alexander <xalexander51@gmail.com>
2021-07-14 18:44:00 -0400

    Add form

--- to_do_project/to_do_app/views.py
+++ to_do_project/to_do_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def user_signup(request):
-    return render(request, 'to_do_app/user_signup.html')
+    return render(request, 'to_do_app/user_signup.html', {'form':UserCreationForm()})
