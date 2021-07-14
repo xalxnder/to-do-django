77887d6458c4b8e586acd290b81d49eba297a108
Xavier Alexander <xalexander51@gmail.com>
2021-07-14 08:23:20 -0400

    Update key

--- to_do_project/to_do/settings.py
+++ to_do_project/to_do/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .key import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-od)!l(2*&8ym27j($jdo9nf1bsndr6v(o(bg3=y&$8dlec9%h9'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
