73eb11106a8fd8e4a7c4bb98702cddabe316a729
Xavier Alexander <xalexander51@gmail.com>
2021-07-21 09:10:34 -0400

    Add error handling

--- to_do_project/to_do_app/views.py
+++ to_do_project/to_do_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 # Create your views here.
 
 def index(request):
     return render(request,'to_do_app/index.html')
+
 def user_signup(request):
     if request.method == 'GET':
         return render(request, 'to_do_app/user_signup.html', {'form':UserCreationForm()})
@@ -12,6 +14,11 @@ def user_signup(request):
         #Check if initial password and confirmation password match
         if request.POST['password1'] == request.POST['password2']:
             #The Post Request(AKA Signing up and creating the User model.)
-            user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
-            user.save()
-            return render(request,'to_do_app/index.html')
+            try:
+                user = User.objects.create_user(request.POST['username'], password = request.POST['password1'])
+                user.save()
+                return render(request,'to_do_app/user_signup.html',{'form':UserCreationForm(), 'success':'User added!'})
+            except IntegrityError:
+                return render(request,'to_do_app/user_signup.html',{'form':UserCreationForm(), 'error':'User name is already taken.'})
+        else:
+            return render(request,'to_do_app/user_signup.html',{'form':UserCreationForm(), 'error':'Passwords do not match'})
